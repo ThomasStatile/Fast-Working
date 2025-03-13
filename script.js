568022fa308e77d4af10217b5f4ac0992750e8bf
Thomas Statile <thomasstatile@gmail.com>
2025-03-13 15:29:26 +0100

    html/chi-siamo.html

---
+++ script.js
@@ -0,0 +1,28 @@
+function openBurgerMenu() {
+    const burgerMenuContent = document.getElementById("burger-menu-content");
+    const closeBurger = document.getElementById("close-burger");
+    const burgerLinks = document.getElementById("burger-links");
+
+    burgerMenuContent.classList.remove("burger-menu-content");
+    closeBurger.classList.remove("close-burger");
+    burgerLinks.classList.remove("burger-links");
+
+    burgerMenuContent.classList.add("burger-menu-content-displayed");
+    closeBurger.classList.add("close-burger-displayed");
+    burgerLinks.classList.add("burger-links-displayed");
+
+}
+
+function closeBurgerMenu() {
+    const burgerMenuContent = document.getElementById("burger-menu-content");
+    const closeBurger = document.getElementById("close-burger");
+    const burgerLinks = document.getElementById("burger-links");
+
+    burgerMenuContent.classList.remove("burger-menu-content-displayed");
+    closeBurger.classList.remove("close-burger-displayed");
+    burgerLinks.classList.remove("burger-links-displayed");
+
+    burgerMenuContent.classList.add("burger-menu-content");
+    closeBurger.classList.add("close-burger");
+    burgerLinks.classList.add("burger-links");
+}
